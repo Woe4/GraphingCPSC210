5121373d910799cf38b9f7333fdf42f2a0ec40a5
June <tyu12@students.cs.ubc.ca>
2023-02-03 13:09:41 -0800

    Finished coordinate class

---
+++ src/main/model/Coordinate.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Coordinate {
+    private int coordX;
+    private int coordY;
+
+
+    public Coordinate(int x, int y) {
+        this.coordX = x;
+        this.coordY = y;
+    }
+
+    public int getCoordX() {
+        return coordX;
+    }
+
+    public int getCoordY() {
+        return coordY;
+    }
+}
